a1542e2c3c3585c854935b45537c6d61d0e7a2c3
kestongregory <Keston@Kestons-MacBook-Pro.local>
2014-10-23 20:54:39 -0400

    fucked up
    should be fine now

---
+++ TECHNICOLOR HYPERDRIVE BETA copy/Assets/Scripts/gameOverScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameOverScript : MonoBehaviour 
+{
+	public GUISkin tryAgainSkin;
+	public GUISkin mainMenuSkin;
+
+	Rect windowRect0 = new Rect(Screen.width/2 - 350, Screen.height/2 - 150, 700, 300);
+	HealthManager health = new HealthManager();
+	ScoreManager score = new ScoreManager();
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{	
+	
+	}
+
+	void DoMyWindow(int windowID) 
+	{
+		GUI.skin = tryAgainSkin;
+
+		if (GUI.Button(new Rect(Screen.width/2 - 525, Screen.height/2 - 400, 400, 200), " "))
+		{
+			Application.LoadLevel("TestingPlayerMove");
+			ScoreManager.score = 0;
+		}
+
+		GUI.skin = mainMenuSkin;
+
+		if (GUI.Button(new Rect(Screen.width/2 - 525, Screen.height/2 - 300, 400, 200), " "))
+		{
+			Application.LoadLevel("mainMenu");
+			print("quit");
+			Time.timeScale = 1.0f;
+			DestroyAllGameObjects();
+		}
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label ("Score: " + ScoreManager.score);
+
+//		if (health.isDead == true)
+//		{
+			windowRect0 = GUI.Window(0, windowRect0, DoMyWindow, "You did so good!");
+//		}
+	}
+
+	public void DestroyAllGameObjects()
+	{
+		GameObject[] GameObjects = (FindObjectsOfType<GameObject>() as GameObject[]);
+		
+		for (int i = 0; i < GameObjects.Length; i++)
+		{
+			Destroy(GameObjects[i]);
+		}
+	}
+}
---
+++ TECHNICOLOR HYPERDRIVE BETA copy/Assets/Scripts/pauseScript.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pauseScript : MonoBehaviour 
+{
+	Rect windowRect0 = new Rect(Screen.width/2 - 350, Screen.height/2 - 150, 700, 300);
+	public bool isPaused = false;
+
+	public GUISkin resumeSkin;
+	public GUISkin mainMenuSkin;
+//
+//	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+//	// Update is called once per frame
+	void Update () 
+	{
+		if (isPaused == false && Input.GetKeyDown("space")) 
+		{
+			Debug.Log("pressed space");
+			Time.timeScale = 0.0f;
+			isPaused = true;
+		}
+
+		else if (isPaused == true && Input.GetKeyDown("space"))
+		{
+			Debug.Log("pressed space");
+			Time.timeScale = 1.0f;
+			isPaused = false;
+		}
+	}
+
+	void DoMyWindow(int windowID) 
+	{
+		GUI.skin = resumeSkin;
+
+		if (GUI.Button(new Rect(Screen.width/2 - 225, Screen.height/2 - 225, 400, 200), " "))
+		{
+			isPaused = false;
+			Time.timeScale = 1.0f;
+			print("Resume");
+		}
+	
+		GUI.skin = mainMenuSkin;
+
+		if (GUI.Button(new Rect(Screen.width/2 - 225, Screen.height/2 - 100, 400, 200), " "))
+		{
+			Application.LoadLevel("mainMenu");
+			print("quit");
+			Time.timeScale = 1.0f;
+			DestroyAllGameObjects();
+		}
+	}
+
+	void OnGUI ()
+	{
+		if (isPaused == true)
+		{
+			windowRect0 = GUI.Window(0, windowRect0, DoMyWindow, "PAUSED");
+		}
+	}
+
+	public void DestroyAllGameObjects()
+	{
+		GameObject[] GameObjects = (FindObjectsOfType<GameObject>() as GameObject[]);
+		
+		for (int i = 0; i < GameObjects.Length; i++)
+		{
+			Destroy(GameObjects[i]);
+		}
+	}
+}
