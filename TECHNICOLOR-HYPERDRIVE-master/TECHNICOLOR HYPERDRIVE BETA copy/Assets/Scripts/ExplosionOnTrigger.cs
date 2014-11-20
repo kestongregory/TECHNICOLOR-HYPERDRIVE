3f56ebba945af4390be1065e92d7180287fb6dfc
Nnythm <johnnyhumberto@bellsouth.net>
2014-11-20 13:46:07 -0500

    Explosion
    Explodes on hit.

--- TECHNICOLOR-HYPERDRIVE-master/TECHNICOLOR HYPERDRIVE BETA copy/Assets/Scripts/ExplosionOnTrigger.cs
+++ TECHNICOLOR-HYPERDRIVE-master/TECHNICOLOR HYPERDRIVE BETA copy/Assets/Scripts/ExplosionOnTrigger.cs
@@ -2,19 +2,21 @@
 using System.Collections;
 
 public class ExplosionOnTrigger : MonoBehaviour {
-	
+
+	public ParticleSystem explosion;
+
 	void Start () {
 		// You can use particleSystem instead of
 		// gameObject.particleSystem.
 		// They are the same, if I may say so
-		particleSystem.emissionRate = 0;
+		//particleSystem.emissionRate = 0;
 	}
 	
 	void Update () {
-		if (Input.GetButtonDown ("Fire1")) {
-			particleSystem.emissionRate = 1000;
-		} else {
-			particleSystem.emissionRate = 0;
+		if (HealthManager.Instance.isHit == true) {
+			Instantiate (explosion, transform.position, Quaternion.identity);
+			explosion.Emit (1000);
+			Debug.Log ("Explode");
 		}
 	}
 }
